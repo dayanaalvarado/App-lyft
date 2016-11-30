714a0b95c35cab04e2e904728ca93827c3b85f49
dayanaalvarado <dayana.alvarado.n@gmail.com>
2016-11-30 17:35:21 -0300

    cargar foto

--- js/seis.js
+++ js/seis.js
@@ -31,3 +31,20 @@ $(document).ready(function(){
 		elink.css("display", "block");
 	});
 });
+
+function previewFile(){
+       var preview = document.querySelector('img'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+  }
+
